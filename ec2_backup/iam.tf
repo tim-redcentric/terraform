5ec8b46160c56da409dcc4767616ba6e89dd52ab
tim-redcentric <tim.samanchi@redcentricplc.com>
2023-08-08 20:41:14 +0100

    initial push

--- ec2_backup/iam.tf
+++ ec2_backup/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "lambda_role" {
-  name = "lambda-ami-backup-role"
+  name = "timsamanchi-lambda-ami-backup-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
