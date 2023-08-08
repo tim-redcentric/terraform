f66db88fc4dcb2dcee5c14a296744e15b1491923
tim-redcentric <tim.samanchi@redcentricplc.com>
2023-08-08 20:20:15 +0100

    added python script for lambda use

---
+++ ec2_backup/lambda.tf
@@ -0,0 +1,7 @@
+resource "aws_lambda_function" "ami_backup_cleanup" {
+  filename      = "lambda_function.zip"
+  function_name = "ami-backup-cleanup"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
+}
